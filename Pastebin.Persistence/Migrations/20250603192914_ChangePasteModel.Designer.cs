627cf276c3e8c5a3cc7c3e6206bfd3f53a379ed6
Volkov Dmitriy <m0nterha@mail.ru>
2025-06-03 22:31:14 +0300

    Add migrations

--- Pastebin.Persistence/Migrations/20250603192914_ChangePasteModel.Designer.cs
+++ Pastebin.Persistence/Migrations/20250603192914_ChangePasteModel.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Pastebin.Persistence;
@@ -11,9 +12,11 @@ using Pastebin.Persistence;
 namespace Pastebin.Persistence.Migrations
 {
     [DbContext(typeof(PastebinDbContext))]
-    partial class PastebinDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250603192914_ChangePasteModel")]
+    partial class ChangePasteModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Pastebin.Persistence/Migrations/20250603192914_ChangePasteModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pastebin.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePasteModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PastePath",
+                table: "Pastes",
+                newName: "UrlHash");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BucketKey",
+                table: "Pastes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BucketKey",
+                table: "Pastes");
+
+            migrationBuilder.RenameColumn(
+                name: "UrlHash",
+                table: "Pastes",
+                newName: "PastePath");
+        }
+    }
+}
