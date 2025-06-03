d894f4ff359a6c047dc31d90777720bd5e324553
Volkov Dmitriy <m0nterha@mail.ru>
2025-06-03 22:19:34 +0300

    Change pastes controller implementation

--- Pastebin.API/Controllers/PastesController.cs
+++ Pastebin.API/Controllers/PastesController.cs
@@ -1,6 +1,5 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Pastebin.API.Models;
 using Pastebin.Application.CQRS.Commands;
 using Pastebin.Application.CQRS.Queries;
 
@@ -12,25 +11,35 @@ namespace Pastebin.API.Controllers
 	public class PastesController : ControllerBase
 	{
 		private readonly IMediator _mediator;
+		private readonly IHttpContextAccessor _httpContextAccessor;
 
-		public PastesController(IMediator mediator)
+		public PastesController(IMediator mediator,
+			IHttpContextAccessor httpContextAccessor)
 		{
 			_mediator = mediator;
+			_httpContextAccessor = httpContextAccessor;
 		}
 
-		[HttpGet("{id}")]
-		public async Task<IActionResult> GetPaste(long id)
+		[HttpGet("{hashUrl}")]
+		public async Task<IActionResult> GetPaste(string hashUrl)
 		{
-			var paste = await _mediator.Send(new GetPasteByIdQuery(id));
+			var paste = await _mediator.Send(new GetPasteByUrlHashQuery(hashUrl));
 			return Ok(paste);
 		}
 
 		[HttpPost]
 		public async Task<IActionResult> CreatePaste([FromBody] CreatePasteCommand command)
 		{
-			var pasteId = await _mediator.Send(command);
-			return Ok(new { Id = pasteId });
+			var urlHash = await _mediator.Send(command);
+
+			var baseUrl = $"{_httpContextAccessor.HttpContext?.Request.Scheme}://" +
+				$"{_httpContextAccessor.HttpContext?.Request.Host}";
+
+			var fullUrl = $"{baseUrl}/pastes/{urlHash}";
+
+			return Ok(new { Url = fullUrl });
 		}
+
 
 		[HttpDelete("{id}")]
 		public async Task<IActionResult> DeletePaste(long id, [FromHeader] Guid authorId)
@@ -39,12 +48,13 @@ namespace Pastebin.API.Controllers
 			return NoContent();
 		}
 
-		[HttpPut("{id}")]
+
+		[HttpPatch("{id}")]
 		public async Task<IActionResult> UpdatePaste( long id, 
 			[FromBody] UpdatePasteCommand command,
 			[FromHeader] Guid authorId)
 		{
-			command = command with { Id = id, AuthorId = authorId };
+			command = command with { Id = id, AuthorId = authorId }; // TODO: Get user id from jwt
 			await _mediator.Send(command);
 			return NoContent();
 		}
